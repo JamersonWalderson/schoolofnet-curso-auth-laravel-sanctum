b216a5f15d7bf6945107b3d653ac6e85d2797704
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2021-12-08 00:32:42 -0300

    aula: Protegendo e testando habilidades

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,12 @@ use Illuminate\Validation\ValidationException;
 |
 */
 
+// [
+//     "1|FI6wkJjaBkgrO8XyseeFYcV9391JJ5WD9iXV30Ht",
+//     "2|uiluYo1Os0BLt94uRUjLCkevUjXd7ZRSyZe0Hunt",
+//     "3|Q4wfTTbNPKXLF4XzDUYWnTl68TIdMpSTYoqXmc04"
+// ]
+
 Route::post('/sanctum/token', function (Request $request) {
     $request->validate([
         'email' => 'required|email',
@@ -31,10 +37,39 @@ Route::post('/sanctum/token', function (Request $request) {
         ]);
     }
 
-    return $user->createToken('post_can_update', ['post:update', 'post:delete'])->plainTextToken;
+    $token_can_all = $user->createToken('can_all')->plainTextToken;
+    $token_can_update = $user->createToken('can_update', ['system:update'])->plainTextToken;
+    $token_can_create = $user->createToken('can_create', ['system:create'])->plainTextToken;
+
+    $abilities = [$token_can_all, $token_can_update, $token_can_create];
+    return $abilities;
 
 });
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('user', function(Request $request){
+        return $request->user();
+    });
+    Route::get('list_tokens', function(Request $request){
+        return $request->user()->tokens;
+    });
+
+    Route::get('token_abilities', function(Request $request){
+        $abilities = [];
+
+        if($request->user()->tokenCan('system:update')) {
+            array_push($abilities, 'Posso atualizar');
+        
+        }
+        if($request->user()->tokenCan('system:create')) {
+            array_push($abilities, 'Posso criar');
+        
+        }
+        if($request->user()->tokenCan('system:all')){
+            array_push($abilities, 'Posso criar e atualizar');
+
+        }
+        return $abilities;
+
+    });
+});
