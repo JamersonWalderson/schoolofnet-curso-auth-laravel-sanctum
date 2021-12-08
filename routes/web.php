cbd9b2a05908bae76bc02f2b3e9868f0b9d3116e
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2021-12-07 23:27:04 -0300

    aula de abilitys

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +14,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
